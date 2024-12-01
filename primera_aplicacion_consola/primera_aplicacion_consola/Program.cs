3fe41ac4a47bdc4c9d585c2a57f55ee34f8630ec
juanse <juansebt.0610@gmail.com>
2024-12-01 17:51:56 -0500

    inicio segundo curso

---
+++ primera_aplicacion_consola/primera_aplicacion_consola/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primera_aplicacion_consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("presiona cualquier tecla para finalizar");
+
+            Console.ReadKey();
+        }
+    }
+}
