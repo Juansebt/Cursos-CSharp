a727c7ca0ce34b58b04c52bbd4fa019f0cd9a086
juanse <juansebt.0610@gmail.com>
2024-12-04 15:51:21 -0500

    variables y tipos de datos 2

--- Csharp1/Program.cs
+++ Csharp1/Program.cs
@@ -17,7 +17,15 @@ namespace Csharp1
             //Variables y tipos de datos
             string Nombre = "Juan Sebastián"; //texto
 
-            MessageBox.Show(Nombre); //mostrar el contenido de la variable
+            int Num1 = 450; //númeero enteros
+            uint Num2 = 500; //número entero pero nunca negativo, solo positivo
+            float Num3 = 35.5f; //número decimal
+            double Num4 = 45.50; //número decimal más grande el float
+            decimal Num5 = 5020.506m; //números decimales grandes
+            byte Num6 = 255; //número pequeño hasta 255
+
+            MessageBox.Show(Nombre); //mostrar el contenido de la variables cuyo contenido es texto
+            MessageBox.Show(Num1.ToString()); //conversión de int a string
 
             //Application.EnableVisualStyles();
             //Application.SetCompatibleTextRenderingDefault(false);
