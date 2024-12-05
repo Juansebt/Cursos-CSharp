4bbf62a6ce384f605481737646292a7c1600e8e5
juanse <juansebt.0610@gmail.com>
2024-12-05 01:27:27 -0500

    tercer formulario - conectar y mostrar datos en un listBox

---
+++ Csharp3/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Csharp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.txtNombres = new System.Windows.Forms.TextBox();
+            this.listNombres = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(49, 364);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(139, 46);
+            this.btnOk.TabIndex = 0;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(210, 364);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(139, 46);
+            this.btnClose.TabIndex = 1;
+            this.btnClose.Text = "CERRAR";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(23, 23);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(69, 20);
+            this.lblNombre.TabIndex = 2;
+            this.lblNombre.Text = "Nombre:";
+            // 
+            // txtNombres
+            // 
+            this.txtNombres.Location = new System.Drawing.Point(108, 16);
+            this.txtNombres.Name = "txtNombres";
+            this.txtNombres.Size = new System.Drawing.Size(267, 26);
+            this.txtNombres.TabIndex = 3;
+            // 
+            // listNombres
+            // 
+            this.listNombres.FormattingEnabled = true;
+            this.listNombres.ItemHeight = 20;
+            this.listNombres.Location = new System.Drawing.Point(27, 73);
+            this.listNombres.Name = "listNombres";
+            this.listNombres.Size = new System.Drawing.Size(348, 224);
+            this.listNombres.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(405, 450);
+            this.Controls.Add(this.listNombres);
+            this.Controls.Add(this.txtNombres);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnOk);
+            this.Name = "Form1";
+            this.Text = "TERCER FORMULARIO";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.TextBox txtNombres;
+        private System.Windows.Forms.ListBox listNombres;
+    }
+}
+
---
+++ Csharp3/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Csharp3
+{
+    public partial class Form1 : Form
+    {
+        List<string> listaNombres = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            string nombre;
+
+            nombre = txtNombres.Text; //obtener el nombre del textBox
+
+            listaNombres.Add(nombre); //agregar un nombre a la lista
+
+            listNombres.DataSource = null; //para refrescar el listBox 
+
+            listNombres.DataSource = listaNombres; //llenar el listBox
+        }
+    }
+}
