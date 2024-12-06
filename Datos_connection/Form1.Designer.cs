0bda3256e41db8522cde97f1dd0016cd7fdd4336
juanse <juansebt.0610@gmail.com>
2024-12-05 22:38:28 -0500

    manejo de archivos - crear

---
+++ Datos_connection/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Datos_connection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEscribir = new System.Windows.Forms.Button();
+            this.btnLeer = new System.Windows.Forms.Button();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnEscribir
+            // 
+            this.btnEscribir.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnEscribir.Location = new System.Drawing.Point(71, 133);
+            this.btnEscribir.Name = "btnEscribir";
+            this.btnEscribir.Size = new System.Drawing.Size(237, 84);
+            this.btnEscribir.TabIndex = 0;
+            this.btnEscribir.Text = "ESCRIBIR";
+            this.btnEscribir.UseVisualStyleBackColor = false;
+            this.btnEscribir.Click += new System.EventHandler(this.btnEscribir_Click);
+            // 
+            // btnLeer
+            // 
+            this.btnLeer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnLeer.Location = new System.Drawing.Point(71, 275);
+            this.btnLeer.Name = "btnLeer";
+            this.btnLeer.Size = new System.Drawing.Size(237, 84);
+            this.btnLeer.TabIndex = 1;
+            this.btnLeer.Text = "LEER";
+            this.btnLeer.UseVisualStyleBackColor = false;
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Location = new System.Drawing.Point(103, 53);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(177, 20);
+            this.lblTitulo.TabIndex = 2;
+            this.lblTitulo.Text = "ARCHIVOS DE TEXTO";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(387, 450);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.btnLeer);
+            this.Controls.Add(this.btnEscribir);
+            this.Name = "Form1";
+            this.Text = "Archivos.txt";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEscribir;
+        private System.Windows.Forms.Button btnLeer;
+        private System.Windows.Forms.Label lblTitulo;
+    }
+}
+
---
+++ Datos_connection/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO; //libreria para trabajar con archivos
+
+namespace Datos_connection
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEscribir_Click(object sender, EventArgs e)
+        {
+            TextWriter Escribe = new StreamWriter("Test.txt"); //creamos un objeto de tipo TextWriter y le asignamos un nombre, este lo guarda en la carpeta raiz
+
+            Escribe.WriteLine("Hola mundo!"); //contenido del archivo
+
+            Escribe.Close(); //cerrar recursos - objeto
+
+            MessageBox.Show("Archvio creado con exito");
+
+            /*
+            string rutaArchivo = @"C:\MiCarpeta\Test.txt";
+
+            // Usar 'using' para asegurar que los recursos se liberen correctamente
+            using (TextWriter Escribe = new StreamWriter(rutaArchivo))
+            {
+                Escribe.WriteLine("Hola mundo!");
+            }
+
+            MessageBox.Show("Archivo creado y contenido escrito en: " + rutaArchivo);
+            */
+        }
+    }
+}
