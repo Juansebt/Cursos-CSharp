6d6e0abf23ce1fc9f00a47ebfc80d935cbef64bb
juanse <juansebt.0610@gmail.com>
2024-12-05 20:13:10 -0500

    POO - creación de una clase

---
+++ POO_practica1/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace POO_practica1
+{
+    public partial class Form1 : Form
+    {
+        Telefono Movistar = new Telefono(); //se instancia un objeto de la clase Telefono
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLlamar_Click(object sender, EventArgs e)
+        {
+            Movistar.Llamar();
+        }
+    }
+}
---
+++ POO_practica1/Telefono.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_practica1
+{
+    public class Telefono
+    {
+        //Atributos o propieades de la clase
+        //son atributos publicos, se pueden usar fuera de la clase
+        public string Marca;
+        public string Color;
+        public string Tipo; 
+
+        //Metodo publico que no recive ningún parámetro
+        public void Llamar()
+        {
+            System.Windows.Forms.MessageBox.Show("Hola a todos...");
+        }
+
+        public void MandarMensaje()
+        {
+
+        }
+    }
+}
